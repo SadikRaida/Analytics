f26f68b6d46381ba88c2ac2a5ef4d83830663c35
calintha <cal.intha77@gmail.com>
2023-07-03 00:54:41 +0200

    Fix build of dashboard

--- front/src/pages/admin/OngletSupport/AdminOnglet.tsx
+++ front/src/pages/admin/OngletSupport/AdminOnglet.tsx
@@ -1,90 +1,72 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Button} from "@mui/material";
-import {useEffect} from "react";
-import AuthService from "../../../services/AuthService";
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 
-const headCells = [
-    {id: 'society', numeric: false, disablePadding: true, label: 'Nom de la société'},
-    {id: 'email', numeric: true, disablePadding: false, label: 'Email'},
-    {id: 'url', numeric: true, disablePadding: false, label: 'Url'},
-    {id: 'actions', numeric: true, disablePadding: false, label: 'Actions'},
-];
+export const FieldsOnglet = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-export const AdminOnglet = () => {
-
-    const [data, setData] = React.useState([])
-    const validateAccount = (id :any) => {
-        console.log(id)
-        AuthService.verifyUser(id).then((res :any) => {
-            getUsers()
-        })
-    }
-
-    const getUsers = () => {
-        AuthService.getUsers().then((res :any) => {
-            setData(res)
-        })
-    }
-
-    useEffect(() => {
-        getUsers()
-    }, [])
+    const validateForm = () => {
+        // Logique de validation du formulaire ici
+    };
 
     return (
-        <>
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            {
-                                headCells.map((headCell) => (
-                                    <TableCell key={headCell.id} align={headCell.numeric ? 'right' : 'left'}>
-                                        {headCell.label}
-                                    </TableCell>
-                                ))
-                            }
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((row :any, key:number) => (
-                            <TableRow
-                                key={key}
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                            >
-                                <TableCell align={'left'}>
-                                    {row?.society}
-                                </TableCell>
-                                <TableCell align={'right'}>
-                                    {row?.email}
-                                </TableCell>
-                                <TableCell align={'right'}>
-                                    {row?.url}
-                                </TableCell>
-                                {
-                                    !row?.isVerified ?
-                                    <TableCell align={'right'}>
-                                        <Button onClick={() => validateAccount(row.id)} style={{
-                                            backgroundColor: '#4caf50',
-                                        }}>
-                                            Vérifier
-                                        </Button>
-                                    </TableCell>
-                                        :
-                                    <TableCell align={'right'}>
-                                    </TableCell>
-                                }
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    )
-}
+        <div>
+            <h2>Indiquez les champs que vous souhaitez ajouter</h2>
+            <Box
+                sx={{
+                    height: '100vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <Grid
+                    display={'flex'}
+                    flexDirection={'column'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    sx={{
+                        width: '30%',
+                    }}
+                >
+                    <TextField
+                        id="email"
+                        label="Email"
+                        color={'secondary'}
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        sx={{
+                            borderRadius: 20,
+                            width: '100%',
+                            marginBottom: 2,
+                        }}
+                    />
+                    <TextField
+                        type={'password'}
+                        id="mot_de_passe"
+                        color={'secondary'}
+                        label="Mot de passe"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        sx={{
+                            borderRadius: 20,
+                            width: '100%',
+                        }}
+                    />
+                    <Button
+                        variant={"outlined"}
+                        onClick={() => validateForm()}
+                        style={{
+                            marginTop: 20,
+                            width: '100%',
+                            backgroundColor: "#00BB7E",
+                        }}
+                    >
+                        Se connecter
+                    </Button>
+                </Grid>
+            </Box>
+        </div>
+    );
+};
--- front/src/pages/admin/Onglets/FieldsOnglet.tsx
+++ front/src/pages/admin/Onglets/FieldsOnglet.tsx
@@ -1,11 +1,17 @@
-import {Box, Button, Grid, TextField} from "@mui/material";
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 
 export const FieldsOnglet = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const validateForm = () => {
+        // Logique de validation du formulaire ici
+    };
+
     return (
         <div>
-            <h2>
-                Indiquez les champs que vous souhaitez ajouter
-            </h2>
+            <h2>Indiquez les champs que vous souhaitez ajouter</h2>
             <Box
                 sx={{
                     height: '100vh',
@@ -13,14 +19,21 @@ export const FieldsOnglet = () => {
                     justifyContent: 'center',
                 }}
             >
-                <Grid display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} sx={{
-                    width: '30%',
-                }}>
+                <Grid
+                    display={'flex'}
+                    flexDirection={'column'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    sx={{
+                        width: '30%',
+                    }}
+                >
                     <TextField
                         id="email"
                         label="Email"
                         color={'secondary'}
                         variant="outlined"
+                        value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         sx={{
                             borderRadius: 20,
@@ -34,21 +47,26 @@ export const FieldsOnglet = () => {
                         color={'secondary'}
                         label="Mot de passe"
                         variant="outlined"
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         sx={{
                             borderRadius: 20,
                             width: '100%',
                         }}
                     />
-                    <Button variant={"outlined"} onClick={() => validateForm()} style={{
-                        marginTop: 20,
-                        width: '100%',
-                        backgroundColor: "#00BB7E",
-                    }}>
+                    <Button
+                        variant={"outlined"}
+                        onClick={() => validateForm()}
+                        style={{
+                            marginTop: 20,
+                            width: '100%',
+                            backgroundColor: "#00BB7E",
+                        }}
+                    >
                         Se connecter
                     </Button>
                 </Grid>
             </Box>
         </div>
-    )
-}
+    );
+};
