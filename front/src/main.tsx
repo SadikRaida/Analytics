d1320cb3dea0bc6bac5dfb7fd689c3be6c44e1c7
louispelarrey <louispelarrey@gmail.com>
2023-07-05 16:54:01 +0200

    browser router change location

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,59 +1,56 @@
 import CssBaseline from "@mui/material/CssBaseline";
-import {createRoot} from 'react-dom/client'
-import Router from './rooter/Router'
-import './index.css'
-import {createTheme, ThemeProvider} from '@mui/material/styles'
+import { createRoot } from "react-dom/client";
+import Router from "./rooter/Router";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AuthProvider from "./providers/AuthProvider.tsx";
-import {BrowserRouter} from "react-router-dom";
-import {StrictMode} from "react";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import SnackbarProvider from "react-mui-snackbar";
 import Header from "./layouts/Header";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#FDFDFD',
-            dark: '#FDFDFD',
-        },
-        secondary: {
-            main: '#282C2B',
-            dark: '#282C2B',
-        },
-        success: {
-            main: '#00A99D',
-            dark: '#00A99D',
-        },
-        info: {
-            main: '#5B98D2',
-            dark: '#5B98D276',
-        },
-        error: {
-            main: '#E53F49',
-            dark: '#E53F49',
-        }
+  palette: {
+    primary: {
+      main: "#FDFDFD",
+      dark: "#FDFDFD",
     },
-    typography: {
-        fontFamily: [
-            'Roboto',
-            'sans-serif',
-        ].join(','),
+    secondary: {
+      main: "#282C2B",
+      dark: "#282C2B",
     },
+    success: {
+      main: "#00A99D",
+      dark: "#00A99D",
+    },
+    info: {
+      main: "#5B98D2",
+      dark: "#5B98D276",
+    },
+    error: {
+      main: "#E53F49",
+      dark: "#E53F49",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto", "sans-serif"].join(","),
+  },
 });
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
-    <StrictMode>
-        <AuthProvider>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <SnackbarProvider>
-                    <BrowserRouter>
-                        <Header/>
-                        <Router/>
-                    </BrowserRouter>
-                </SnackbarProvider>
-            </ThemeProvider>
-        </AuthProvider>
-    </StrictMode>,
-)
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SnackbarProvider>
+            <Header />
+            <Router />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
